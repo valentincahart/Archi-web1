5e095939e992b768041919a61bd81cb783873a26
Sébastien Linck <55988723+sebastienlinck@users.noreply.github.com>
2019-12-10 13:27:30 +0100

    Add files via upload

--- commentaires.php
+++ commentaires.php
@@ -6,7 +6,7 @@
 	<body>
 		<form method="POST">
 			<input type="text" name="nom" placeholder="Nom"><br>
-			<textarea name="comment" placeholder="Commentaire"></textarea><br>
+			<textarea name="contenu" placeholder="Commentaire"></textarea><br>
 			<input type="submit" value="Envoyer" name="envoyer">
 		</form>
 		<?php
@@ -14,8 +14,8 @@
 			if(isset($_POST['envoyer']))
 			{
 				$nom=trim(htmlentities(mysqli_real_escape_string($lien,$_POST['nom'])));
-				$comment=trim(htmlentities(mysqli_real_escape_string($lien,$_POST['comment'])));
-				$req="INSERT INTO comment VALUES (NULL,'$nom','$comment')";
+				$contenu=trim(htmlentities(mysqli_real_escape_string($lien,$_POST['contenu'])));
+				$req="INSERT INTO comment VALUES (NULL,'$nom','$contenu')";
 				$res=mysqli_query($lien,$req);
 				if(!$res)
 				{
@@ -31,7 +31,7 @@
 			{
 				$page=$_GET['page'];
 			}
-			$commparpage=2;
+			$commparpage=3;
 			$premiercomm=$commparpage*($page-1);
 			$req="SELECT * FROM comment ORDER BY id LIMIT $premiercomm,$commparpage";/* LIMIT dit ou je commence et combien j'en prends*/
 			$res=mysqli_query($lien,$req);
@@ -58,16 +58,16 @@
 			{
 				$nbcomm=mysqli_num_rows($res); // Retourne le nombre de lignes dans un résultat. 
 				$liste=mysqli_fetch_array($res);
-				$nbpages=ceil($liste['nbcomm']/$commparpage); /*Ceil arrondit a l'entier supérieur*/
+				$nbpages=ceil($nbcomm/$commparpage); /*Ceil arrondit a l'entier supérieur*/
 				echo "<br> Pages : ";
 				echo "<a href='commentaires.php?page=1'> << </a>";
-				echo "<a href='commentaires.php?page=($page-1)'> < </a>";
+				echo "<a href='commentaires.php?page=(".($page-1).")'> < </a>";
 				for($i=($page-2);$i<=($page+2);$i++)
 				{
 					echo "<a href='commentaires.php?page=$i'> $i </a>";
 				}
 			}
-			echo "<a href='commentaires.php?page=($page+1)'> > </a>";
+			echo "<a href='commentaires.php?page=".($page+1)."'> > </a>";
 			echo "<a href='commentaires.php?page=$nbpages'> >> </a>";
 			
 			mysqli_close($lien);
